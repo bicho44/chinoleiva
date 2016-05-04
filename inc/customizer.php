8fabc6d70da4b04418a0e04d56dda7f50f86cc9e
Federico <admin@imgdigital.com.ar>
2016-05-04 00:58:20 -0300

    tema inicial con el sistema IMGD

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Chino Leiva Theme Customizer.
+ *
+ * @package Chino_Leiva
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function chinoleiva_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'chinoleiva_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function chinoleiva_customize_preview_js() {
+	wp_enqueue_script( 'chinoleiva_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'chinoleiva_customize_preview_js' );
