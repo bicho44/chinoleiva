bcee46ed687f052cb22b54d9e14a83b855aafb33
Federico <admin@imgdigital.com.ar>
2016-06-16 20:10:44 -0300

    mensajes y menú que no andaba

--- front-page.php
+++ front-page.php
@@ -11,14 +11,39 @@ get_header();
 /*  Hooking here  */
 //do_action('imgd_post_header');
 ?>
-	<!--Arrow Navigation-->
-	<a id="prevslide" class="load-item"></a>
-	<a id="nextslide" class="load-item"></a>
+<?php
+if ( have_posts() ) :
 
-	<!--Time Bar-->
-	<div id="progress-back" class="load-item">
-		<div id="progress-bar"></div>
+	?>
+	<div class="contenido">
+	<header>
+		<h1 class="page-title "><?php single_post_title(); ?></h1>
+	</header>
+	<?php
+
+
+	/* Start the Loop */
+	while ( have_posts() ) : the_post();
+
+		the_content();
+
+		/*
+		 * Include the Post-Format-specific template for the content.
+		 * If you want to override this in a child theme, then include a file
+		 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+		 */
+		//get_template_part( 'template-parts/content', get_post_format() );
+
+	endwhile;
+
+	the_posts_navigation();
+
+else :
+
+	get_template_part( 'template-parts/content', 'none' );
+
+endif;
+?>
 	</div>
-
 <?php
 get_footer();
